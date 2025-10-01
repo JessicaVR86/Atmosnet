1896f8b9b0f583db0f1e63e0d0f62888eb8cc16e
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-10-01 17:34:21 +0200

    html structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,63 @@
-import '../styles/App.scss';
+import "../styles/App.scss";
+import { useState } from "react";
+
+
+
 
 function App() {
+  const [ photo, setPhoto ] = useState( "" );
+  const [ image, setImage ] = useState( "" );
+  const [ btnName, setBtnName ] = useState( "" );
+  const [ linkName, setLinkName ] = useState( "" );
+  const [ listLinks, setListLinks ] = useState( [] );
+
+
+
+
+
+
+
+
+
   return (
-    <p>Escribe aquí tu código HTML</p>
+    <>
+      <header>
+        <h1>Atmosnet</h1>
+        <h3>One moment. One link. Let's stay close.</h3>
+      </header>
+
+      <main>
+        <div>
+          <label>
+            Add profile photo
+            <input type="file" />
+          </label>
+        </div>
+
+        <div>
+          <label htmlFor="">
+            Add button image
+            <input type="file" />
+          </label>
+        </div>
+        <div>
+          <input type="text" placeholder="Name your button" />
+          <input type="text" placeholder="Add your link" />
+          <button className="create">Create your link</button>
+        </div>
+      </main>
+
+      <section>
+        <div>
+          <img/> 
+        </div>
+        <div>
+          <a href="">
+            <img   />
+          </a>
+        </div>
+      </section>
+    </>
   );
 }
 
