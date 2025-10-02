0b0e29efd82bb936522540e08a8de56ca14bc559
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-10-02 15:50:38 +0200

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,52 @@
 import "../styles/App.scss";
 import { useState } from "react";
 
-
-
-
 function App() {
-  const [ photo, setPhoto ] = useState( "" );
-  const [ image, setImage ] = useState( "" );
-  const [ btnName, setBtnName ] = useState( "" );
-  const [ linkName, setLinkName ] = useState( "" );
-  const [ listLinks, setListLinks ] = useState( [] );
+  const [photo, setPhoto] = useState("");
+  const [image, setImage] = useState("");
+  const [btnName, setBtnName] = useState("");
+  const [linkName, setLinkName] = useState("");
+  const [listLinks, setListLinks] = useState([]);
 
+  const handleInputPhoto = (ev) => {
+    const photoFile = ev.target.files[0];
+    if (photoFile) {
+      const profilePhoto = URL.createObjectURL(photoFile);
+      setPhoto(profilePhoto);
+    }
+  };
 
+  const handleInputImage = (ev) => {
+    const imageBtn = ev.target.files[0];
+    if (imageBtn) {
+      const picBtn = URL.createObjectURL(imageBtn);
+      setImage(picBtn);
+    }
+  };
 
+  const handleBtnName = (ev) => {
+    setBtnName(ev.target.value);
+  };
 
-const handleInputPhoto = (ev) =>{
+  const handleLinkName = (ev) => {
+    setLinkName(ev.target.value);
+  };
 
-  const photoFile = ev.target.files[0];
-  if (photoFile){
-    const profilePhoto = URL.createObjectURL(photoFile);
-    setPhoto(profilePhoto)
-  }
-};
-
-const handleInputImage = (ev) =>{
-
-  const imageBtn = ev.target.files[0];
-  if (imageBtn){
-    const picBtn = URL.createObjectURL(picBtn);
-    setPhoto(picBtn)
-  }
-};
-
-
-
+  const handleAddLinks = () => {
+    if (btnName && linkName) {
+      setListLinks([
+        ...listLinks,
+        { picture: image, btn: btnName, url: linkName },
+      ]);
+      setImage("");
+      setBtnName("");
+      setLinkName("");
+    }
+  };
 
   return (
     <>
-      <header>
+      <header >
         <h1>Atmosnet</h1>
         <h3>One moment. One link. Let's stay close.</h3>
       </header>
@@ -46,31 +55,62 @@ const handleInputImage = (ev) =>{
         <div>
           <label>
             Add profile photo
-           <input type="file" className="hidden_input"   onChange={handleInputPhoto} />
+            <input
+              type="file"
+              className="hidden_input"
+              onChange={handleInputPhoto}
+            />
           </label>
         </div>
 
         <div>
-          <label htmlFor="">
+          <label >
             Add button image
-            <input type="file"   className="hidden_input"  onChange={handleInputImage} />
+            <input
+              type="file"
+              className="hidden_input"
+              onChange={handleInputImage}
+            />
           </label>
         </div>
         <div>
-          <input type="text" placeholder="Name your button" />
-          <input type="text" placeholder="Add your link" />
-          <button className="create">Create your link</button>
+          <input
+            type="text"
+            placeholder="Name your button"
+            onChange={handleBtnName}
+          />
+          <input
+            type="text"
+            placeholder="Add your link"
+            onChange={handleLinkName}
+          />
+          <button className="create" onClick={handleAddLinks}>
+            {" "}
+            Create your link
+          </button>
         </div>
       </main>
 
       <section>
         <div>
-          <img />
+          {photo && (
+            <img src={photo} alt="foto de perfil" className="myPhoto" />
+          )}
         </div>
-        <div>
-          <a href="">
-            <img />
-          </a>
+        <div className="linkList">
+          {listLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link_btn"
+            >
+              {link.picture && <img src={link.picture} alt={link.btn} className="btn_picture"/>}
+
+             <p className="link_btn">{link.btn}</p> 
+            </a>
+          ))}
         </div>
       </section>
     </>
